764a5d1850fd446bd82a55e06ad5a22b25145f4a
fan.wenqian <wqfan1231@sina.com>
2021-08-03 11:32:17 +0800

    add 测试StringUtils的join()

--- src/com/fan/spring5/aop/xml/BookTest.java
+++ src/com/fan/spring5/aop/xml/BookTest.java
@@ -1,10 +1,13 @@
 package com.fan.spring5.aop.xml;
 
+import org.apache.commons.lang3.StringUtils;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.text.DecimalFormat;
+import java.util.Arrays;
+import java.util.List;
 
 public class BookTest {
 
@@ -21,4 +24,11 @@ public class BookTest {
 //        String format = decimalFormat.format(121.111);
 //        System.out.println(format);
 //    }
+
+    @Test
+    public void test3(){
+        List<String> list = Arrays.asList("0", "1", "2");
+        String str = StringUtils.join(list);
+        System.out.println(str.substring(1,str.length()-1));
+    }
 }
